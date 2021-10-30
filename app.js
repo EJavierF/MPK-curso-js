b7240abfd8cd0114de7a124b0eb1e789f063298a
EJavierF <javierfalcon86@gmail.com>
2021-10-30 17:31:20 -0300

    vC -- Listo para entrega desafío clase 9

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 /*******************************************/
-/****PRIMER PRE-ENTREGA DE TRABAJO FINAL****/
+/****SEGUNDA PRE-ENTREGA DE TRABAJO FINAL****/
 /*PLANTILLA DE INGRESO DE ARTICULOS DE BLOG*/
 /*******************************************/
 
+import { articulos } from "./articulosData.js";
+
 class articulo {
-    constructor(titulo, tema, autor, fecha) {
+    constructor(id, titulo, tema, autor, fecha) {
+        this.id = id;
         this.titulo = titulo;
         this.tema = tema;
         this.autor = autor;
@@ -25,8 +28,26 @@ const crearArticulo = (artNuevo) => {
     localStorage.setItem("articulosLista", JSON.stringify(articulosLista));
 };
 
+//Buscando un artículo por id
+const buscaArticulo = (id) => {
+    const nota = articulosLista.find((nota) => nota.id === id);
+
+    if (!nota) {
+        throw new Error(`No existe el artículo con id ${id}`);
+    }
+    return nota;
+};
+
+//Borrando artículo
+const borrarArticulo = (id) => {
+    const art = buscaArticulo(id);
+    const index = articulosLista.indexOf(art);
+    articulosLista.splice(index, 1);
+    localStorage.setItem("articulosLista", JSON.stringify(articulosLista));
+};
+
 //vector con lista completa de articulos
-let articulosLista = JSON.parse(localStorage.getItem("articulosLista")) || [];
+let articulosLista = JSON.parse(localStorage.getItem("articulosLista")) || articulos;
 
 // traigo los elementos del DOM
 const formArticulo = document.getElementById("form-articulo");
@@ -36,6 +57,8 @@ const ingAutor = document.getElementById("autor");
 const ingFecha = document.getElementById("fecha");
 const formSubscribir = document.getElementById("form-subscribir");
 const listaUltimasEntradas = document.getElementById("lista-ultimas-entradas");
+const listaArtDOM = document.getElementById("lista-articulos");
+const btnBorrarArt = document.getElementById("btn-borrar-art");
 
 //funcion para listar los nombres de articulos
 const mostrarArticulos = () => {
@@ -61,8 +84,9 @@ formArticulo.addEventListener("submit", (event) => {
     const tema = ingTema.value;
     const autor = ingAutor.value;
     const fecha = ingFecha.value;
+    const id = titulo[0] + tema[0] + autor[0] + fecha;
 
-    const art01 = new articulo(titulo, tema.toLowerCase(), autor.toLowerCase(), new Date(fecha));
+    const art01 = new articulo(id, titulo, tema.toLowerCase(), autor.toLowerCase(), new Date(fecha));
     crearArticulo(art01);
     alert("Se ha creado tu entrada");
 });
@@ -76,6 +100,29 @@ for (let art of articulosOrdenados) {
     itemArticulo.textContent = `${art.titulo}`;
     listaUltimasEntradas.appendChild(itemArticulo);
 }
+
+const renderLista = () => {
+    for (let art of articulosLista) {
+        let itemArticulo = document.createElement("li");
+        itemArticulo.innerHTML = `<input type="checkbox" id="${art.id}">    ${art.titulo}
+        `;
+        listaArtDOM.appendChild(itemArticulo);
+    }
+};
+
+btnBorrarArt.addEventListener("click", () => {
+    for (let art of articulosLista) {
+        const checkbox = document.getElementById(art.id);
+        if (checkbox.checked) {
+            borrarArticulo(art.id);
+        }
+    }
+    location.reload();
+});
+
+//Render de lista en DOM
+
+renderLista();
 
 //Muestro todo el array de objetos
 console.log(articulosLista);
