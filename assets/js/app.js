8494f3ba67fe531c2986ddf6acfee3fcd12f11b4
EJavierF <javierfalcon86@gmail.com>
2021-11-03 23:50:55 -0300

    pre-entrega lista para entregar y revisar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,56 +1,11 @@
 /*******************************************/
 /****SEGUNDA PRE-ENTREGA DE TRABAJO FINAL****/
-/*PLANTILLA DE INGRESO DE ARTICULOS DE BLOG*/
+/*PLANTILLA DE CREACION DE ARTICULOS DE BLOG*/
 /*******************************************/
 
 import { articulos } from './articulosData.js';
-
-class articulo {
-    constructor(id, titulo, tema, autor, fecha, url) {
-        this.id = id;
-        this.titulo = titulo;
-        this.tema = tema;
-        this.autor = autor;
-        this.fecha = fecha;
-        this.url = url;
-        this.fechaCarga = new Date();
-    }
-
-    describir() {
-        alert(
-            `El título del artículo es: ${this.titulo} \n Clasificado como: ${this.tema} \n Creado por: ${this.autor} \n Escrito el día: ${this.fecha} \n Cargado en el sitio el: ${this.fechaCarga}`
-        );
-    }
-}
-
-const armaUrl = (cadena) => {
-    const esp = / /g;
-    return cadena.toLowerCase().replace(esp, '-');
-};
-
-// funcion para solicitar al usuario que ingrese los datos de un objeto nuevo
-const crearArticulo = (artNuevo) => {
-    articulosLista.push(artNuevo);
-    localStorage.setItem('articulosLista', JSON.stringify(articulosLista));
-};
-
-//Buscando un artículo por id
-const buscaArticulo = (id) => {
-    const nota = articulosLista.find((nota) => nota.id === id);
-
-    if (!nota) {
-        throw new Error(`No existe el artículo con id ${id}`);
-    }
-    return nota;
-};
-
-//Borrando artículo
-const borrarArticulo = (id) => {
-    const art = buscaArticulo(id);
-    const index = articulosLista.indexOf(art);
-    articulosLista.splice(index, 1);
-    localStorage.setItem('articulosLista', JSON.stringify(articulosLista));
-};
+import { articulo } from './clases.js';
+import { armaUrl, crearArticulo, buscaArticulo, borrarArticulo, mostrarArticulos, ordenaFecha, renderLista } from './funciones.js';
 
 //vector con lista completa de articulos
 let articulosLista = JSON.parse(localStorage.getItem('articulosLista')) || articulos;
@@ -70,21 +25,6 @@ const listaArtDOM = document.getElementById('lista-articulos');
 const btnBorrarArt = document.getElementById('btn-borrar-art');
 const checkOrd = document.getElementById('check-ord');
 
-//funcion para listar los nombres de articulos
-const mostrarArticulos = () => {
-    console.log('Lista de articulos en crudo');
-    for (const art of articulosLista) {
-        console.log(art.titulo);
-    }
-};
-//Ordenando por fecha
-const ordenaFecha = (lista) => {
-    const artOrdFecha = lista.slice().sort((a, b) => new Date(b.fecha).getTime() > new Date(a.fecha).getTime());
-    console.log('Se ordenaron los articulos por fecha decreciente');
-
-    return artOrdFecha;
-};
-
 // Funcion para crea articulo y lo agrega al array, cuando se pulsa el boton
 formArticulo.addEventListener('submit', (event) => {
     const titulo = ingTitulo.value;
@@ -95,7 +35,7 @@ formArticulo.addEventListener('submit', (event) => {
     const url = `${ruta}${armaUrl(titulo)}.html`;
 
     const art01 = new articulo(id, titulo, tema.toLowerCase(), autor.toLowerCase(), new Date(fecha), url);
-    crearArticulo(art01);
+    crearArticulo(art01, articulosLista);
     alert('Se ha creado tu entrada');
 });
 
@@ -110,26 +50,11 @@ for (let i = 0; i < articulosOrdenados.length && i < 5; i++) {
     listaUltimasEntradas.appendChild(itemArticulo);
 }
 
-const renderLista = (a) => {
-    let lista = '';
-    if (a == true) {
-        lista = articulosOrdenados;
-    } else {
-        lista = articulosLista;
-    }
-    for (let art of lista) {
-        let itemArticulo = document.createElement('li');
-        itemArticulo.innerHTML = `<input type="checkbox" id="${art.id}">    ${art.titulo}
-        `;
-        listaArtDOM.appendChild(itemArticulo);
-    }
-};
-
 btnBorrarArt.addEventListener('click', () => {
     for (let art of articulosLista) {
         const checkbox = document.getElementById(art.id);
         if (checkbox.checked) {
-            borrarArticulo(art.id);
+            borrarArticulo(art.id, articulosLista);
         }
     }
     location.reload();
@@ -137,7 +62,7 @@ btnBorrarArt.addEventListener('click', () => {
 
 //Render de lista en DOM todavía sin opción de mostrar ordenado por lo menos en el cuerpo (sí esta en aside)
 
-renderLista(false);
+renderLista(false, articulosLista, articulosOrdenados, listaArtDOM);
 
 //Subscripción
 formSubscribir.addEventListener('submit', (event) => {
---
+++ assets/js/funciones.js
@@ -0,0 +1,62 @@
+// Arma las URL a partir de la nueva entrada
+const armaUrl = (cadena) => {
+    const esp = / /g;
+    return cadena.toLowerCase().replace(esp, '-');
+};
+
+// Actualizo array de artículos
+const crearArticulo = (artNuevo, listaCruda) => {
+    listaCruda.push(artNuevo);
+    localStorage.setItem('articulosLista', JSON.stringify(listaCruda));
+};
+
+//Buscando un artículo por id
+const buscaArticulo = (id, articulosLista) => {
+    const nota = articulosLista.find((nota) => nota.id === id);
+
+    if (!nota) {
+        throw new Error(`No existe el artículo con id ${id}`);
+    }
+    return nota;
+};
+
+//Borrar artículo por id
+const borrarArticulo = (id, articulosLista) => {
+    const art = buscaArticulo(id);
+    const index = articulosLista.indexOf(art);
+    articulosLista.splice(index, 1);
+    localStorage.setItem('articulosLista', JSON.stringify(articulosLista));
+};
+
+//funcion para listar los nombres de articulos
+const mostrarArticulos = (articulosLista) => {
+    console.log('Lista de articulos en crudo');
+    for (const art of articulosLista) {
+        console.log(art.titulo);
+    }
+};
+//Ordenando por fecha
+const ordenaFecha = (lista) => {
+    const artOrdFecha = lista.slice().sort((a, b) => new Date(b.fecha).getTime() > new Date(a.fecha).getTime());
+    console.log('Se ordenaron los articulos por fecha decreciente');
+
+    return artOrdFecha;
+};
+
+//Muestra la lista en el DOM
+const renderLista = (a, listaCruda, listaOrd, listaArtDOM) => {
+    let lista = '';
+    if (a == true) {
+        lista = listaOrd;
+    } else {
+        lista = listaCruda;
+    }
+    for (let art of lista) {
+        let itemArticulo = document.createElement('li');
+        itemArticulo.innerHTML = `<input type="checkbox" id="${art.id}">    ${art.titulo}
+      `;
+        listaArtDOM.appendChild(itemArticulo);
+    }
+};
+
+export { armaUrl, crearArticulo, buscaArticulo, borrarArticulo, mostrarArticulos, ordenaFecha, renderLista };
